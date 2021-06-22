b4e7ddbe3e28d6cb140efa20a5322086daef9c31
tanangular <tanangular@gmail.com>
2021-06-22 23:39:57 +0700

    first commit

---
+++ windi.config.js
@@ -0,0 +1,9 @@
+export default {
+  shortcuts: {
+    'btn': 'py-2 px-4 rounded-lg shadow-md bg-white border border-2 my-8',
+    'bg-pale-grey': 'text-white bg-pink-500',
+  },
+  alias: {
+    'tan': 'bg-blue-300'
+  }
+}
