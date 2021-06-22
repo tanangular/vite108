af18cf86a3203a6bcd7ff7bed7aae5eb5799df23
tanangular <tanangular@gmail.com>
2021-06-22 23:44:27 +0700

    add 600

--- windi.config.js
+++ windi.config.js
@@ -1,7 +1,7 @@
 export default {
   shortcuts: {
     'btn': 'py-2 px-4 rounded-lg shadow-md bg-white border border-2 my-8',
-    'bg-pale-grey': 'text-white bg-pink-500',
+    'bg-pale-grey': 'text-white bg-pink-600',
   },
   alias: {
     'tan': 'bg-blue-300'
